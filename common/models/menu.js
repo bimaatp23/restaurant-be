e16bb2db1d6ac265b66355afd52a9728ae9e84f6
Bima Tribuana Putra <bimaatp.23@gmail.com>
2024-04-16 15:20:01 +0700

    Add authentication to menu service

--- common/models/menu.js
+++ common/models/menu.js
@@ -1,12 +1,15 @@
 const { uuid } = require('../../server/utils/UUID')
 const db = require('../../server/utils/DB')
+const { response, error_response } = require('../../server/utils/Response')
+const authentication = require('../../server/utils/Authentication')
+const Constant = require('../../server/utils/Constant')
 
 module.exports = function (Menu) {
     // GET
     Menu.get = function (callback) {
         db.connect((err, client, done) => {
             if (err) {
-                callback(err)
+                callback(null, error_response(err.message))
                 return
             }
             client.query(
@@ -14,10 +17,17 @@ module.exports = function (Menu) {
                 (err, result) => {
                     done()
                     if (err) {
-                        callback(err)
+                        callback(null, error_response(err.message))
                     } else {
-                        const menu = result.rows
-                        callback(null, menu)
+                        const menu = result.rows.map((data) => {
+                            return {
+                                id: data.id,
+                                name: data.name,
+                                description: data.description,
+                                price: parseFloat(data.price)
+                            }
+                        })
+                        callback(null, response(200, 'Get Menu List Success', menu))
                     }
                 }
             )
@@ -25,66 +35,70 @@ module.exports = function (Menu) {
     }
     Menu.remoteMethod('get', {
         http: { verb: 'get', path: '/' },
-        returns: { arg: 'menu', type: 'array', root: true }
+        returns: { arg: 'menu', type: 'object', root: true }
     })
 
     // POST
-    Menu.create = function (data, callback) {
-        const { name, description, price } = data
+    Menu.create = function (name, description, price, callback) {
         db.connect((err, client, done) => {
             if (err) {
-                callback(err)
+                callback(null, error_response(err.message))
                 return
             }
             client.query(
-                'INSERT INTO public."menus" (id, name, description, price) VALUES ($1, $2, $3, $4) RETURNING *',
+                'INSERT INTO public."menus" (id, name, description, price) VALUES ($1, $2, $3, $4)',
                 [uuid(), name, description, price],
                 (err, result) => {
                     done()
                     if (err) {
-                        callback(err)
+                        callback(null, error_response(err.message))
                     } else {
-                        const menu = result.rows[0]
-                        callback(null, menu)
+                        callback(null, response(200, 'Create Menu Success'))
                     }
                 }
             )
         })
     }
+    Menu.beforeRemote('create', (ctx, unused, next) => authentication(ctx, unused, next, Constant.ROLE_ADMIN))
     Menu.remoteMethod('create', {
         http: { verb: 'post', path: '/' },
-        accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
+        accepts: [
+            { arg: 'name', type: 'string', http: { source: 'formData' } },
+            { arg: 'description', type: 'string', http: { source: 'formData' } },
+            { arg: 'price', type: 'number', http: { source: 'formData' } }
+        ],
         returns: { arg: 'menu', type: 'object', root: true }
     })
 
     // PUT
-    Menu.update = function (id, data, callback) {
-        const { name, description, price } = data
+    Menu.update = function (id, name, description, price, callback) {
         db.connect((err, client, done) => {
             if (err) {
-                callback(err)
+                callback(null, error_response(err.message))
                 return
             }
             client.query(
-                'UPDATE public."menus" SET name = $1, description = $2, price = $3 WHERE id = $4 RETURNING *',
+                'UPDATE public."menus" SET name = $1, description = $2, price = $3 WHERE id = $4',
                 [name, description, price, id],
                 (err, result) => {
                     done()
                     if (err) {
-                        callback(err)
+                        callback(null, error_response(err.message))
                     } else {
-                        const menu = result.rows[0]
-                        callback(null, menu)
+                        callback(null, response(200, 'Update Menu Success'))
                     }
                 }
             )
         })
     }
+    Menu.beforeRemote('update', (ctx, unused, next) => authentication(ctx, unused, next, Constant.ROLE_ADMIN))
     Menu.remoteMethod('update', {
         http: { verb: 'put', path: '/:id' },
         accepts: [
             { arg: 'id', type: 'string', required: true },
-            { arg: 'data', type: 'object', http: { source: 'body' } }
+            { arg: 'name', type: 'string', http: { source: 'formData' } },
+            { arg: 'description', type: 'string', http: { source: 'formData' } },
+            { arg: 'price', type: 'number', http: { source: 'formData' } }
         ],
         returns: { arg: 'menu', type: 'object', root: true }
     })
@@ -93,24 +107,24 @@ module.exports = function (Menu) {
     Menu.delete = function (id, callback) {
         db.connect((err, client, done) => {
             if (err) {
-                callback(err)
+                callback(null, error_response(err.message))
                 return
             }
             client.query(
-                'DELETE FROM public."menus" WHERE id = $1 RETURNING *',
+                'DELETE FROM public."menus" WHERE id = $1',
                 [id],
                 (err, result) => {
                     done()
                     if (err) {
-                        callback(err)
+                        callback(null, error_response(err.message))
                     } else {
-                        const menu = result.rows[0]
-                        callback(null, menu)
+                        callback(null, response(200, 'Delete Menu Success'))
                     }
                 }
             )
         })
     }
+    Menu.beforeRemote('delete', (ctx, unused, next) => authentication(ctx, unused, next, Constant.ROLE_ADMIN))
     Menu.remoteMethod('delete', {
         http: { verb: 'delete', path: '/:id' },
         accepts: { arg: 'id', type: 'string', required: true },
