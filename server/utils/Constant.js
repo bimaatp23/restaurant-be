22f5794ba5c834248f313e8bd4067064e1d3f28d
Bima Tribuana Putra <bimaatp.23@gmail.com>
2024-04-20 12:27:29 +0700

    Update constant util

--- server/utils/Constant.js
+++ server/utils/Constant.js
@@ -1,6 +1,10 @@
 class Constant {
     static ROLE_ADMIN = 'admin'
     static ROLE_CUSTOMER = 'customer'
+
+    static STATUS_PENDING = 'PENDING'
+    static STATUS_PROCESS = 'PROCESS'
+    static STATUS_DONE = 'DONE'
 }
 
 module.exports = Constant
